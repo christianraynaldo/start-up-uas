9ac4c62f167ec9e9c95cdbe40975ee5e2814dafe
fachryatta <fachryatallah123@gmail.com>
2026-07-13 23:57:16 +0700

    Improve UI homepage and redesign impact section

--- script.js
+++ script.js
@@ -24,3 +24,84 @@ document.querySelector(".contact-form").addEventListener("submit", function (e) 
   e.preventDefault();
   alert("Terima kasih! Pesan Anda sudah terkirim ke ReWear Studio.");
 });
+
+let cart = [];
+
+function toggleCart(){
+
+document.getElementById("cartSidebar").classList.toggle("open");
+
+}
+
+function addToCart(name,price){
+
+cart.push({
+name,
+price
+});
+
+updateCart();
+
+}
+
+function updateCart(){
+
+const container=document.getElementById("cartItems");
+
+const total=document.getElementById("cartTotal");
+
+const count=document.getElementById("cartCount");
+
+container.innerHTML="";
+
+let totalHarga=0;
+
+cart.forEach((item,index)=>{
+
+totalHarga+=item.price;
+
+container.innerHTML+=`
+
+<div class="cart-item">
+
+<div>
+
+<strong>${item.name}</strong>
+
+<br>
+
+Rp${item.price.toLocaleString('id-ID')}
+
+</div>
+
+<button onclick="removeItem(${index})">
+
+❌
+
+</button>
+
+</div>
+
+`;
+
+});
+
+if(cart.length===0){
+
+container.innerHTML="<p class='empty-cart'>Keranjang masih kosong.</p>";
+
+}
+
+count.innerHTML=cart.length;
+
+total.innerHTML="Rp"+totalHarga.toLocaleString('id-ID');
+
+}
+
+function removeItem(index){
+
+cart.splice(index,1);
+
+updateCart();
+
+}
